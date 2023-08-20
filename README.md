405652a5381613f37f860352c8459d76a381823d
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2023-08-20 12:02:19 +0200

    Should work™

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # pihole-doh-docker
 Simple setup for Pi-Hole with DoH using Docker for Linux.
+
+## Prerequisites
+- Static Local IP Address
+- Domain name pointed to your server (this setup relies on it)
+
+## Setup
+1. Set up a static local IP address for your server
+2. Have a domain name or subdomain for pihole pointing to your server.
+   This is needed for TLS to work. Traefik also relies on this to access the Pi-Hole dashboard.
+3. Install (Docker)[https://docs.docker.com/engine/install/]
+4. Clone this repository in the directory of your choice
+5. Create the .env file `cp .env.example .env`
+6. Edit the `.env` file and change the values to your liking
+7. `sudo chmod +x start.sh && sudo ./start.sh`
+8. Access pi-hole at `http://<your pihole domain name>`
+9. Set router or computer's DNS to point to your Pi-Hole's IP address
+
+## Usage
+- `sudo ./start.sh` - Starts the containers
+- `sudo ./stop.sh` - Stops the containers
+
+## Troubleshooting
+### Port 53 is already claimed
+If you have another DNS server running on port 53, you will need to stop it before running this setup.
+
+Check which process, if any is claiming port 53. If it's empty, you're good to go.
+```bash
+sudo lsof -i :53
+```
+
+#### systemd-resolved claimed
+If systemd-resolved is claiming port 53, you will need to disable it.
+
+```bash
+sudo nano /etc/systemd/resolved.conf
+```
+
+Set this line (uncomment if needed):
+
+```conf
+DNSStubListener=no
+```
+
+```bash
+# Restart systemd-resolved
+sudo systemctl restart systemd-resolved
+
+# Check if still claimed
+sudo lsof -i :53
+```
---
+++ start.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "Error: This script must be run as root (use sudo). Exiting..."
+    exit 1
+fi
+
+# Docker check
+if ! command -v docker &> /dev/null; then
+    echo "Error: Docker is not installed. Install it using the official instructions for your OS"
+    echo "https://docs.docker.com/engine/install/"
+    exit 1
+fi
+
+# .env checks
+# Check if .env doesn't exist
+if [ ! -f .env ]; then
+    cp .env.example .env
+    echo "Error: .env file was copied from .env.example. Please set up the .env file before proceeding."
+    exit 1
+fi
+
+# Check if EMAIL value in .env is set to "neededfor@letsencrypt.org"
+if grep -q '^EMAIL=neededfor@letsencrypt.org$' .env; then
+    echo "Error: .env file is not configured. Please update the value for your setup."
+    exit 1
+fi
+
+# Create data dir
+if [ ! -d ./data ]; then
+    mkdir ./data
+fi
+
+# Check if port 53 is claimed by another process
+if sudo ss -tuln | grep -q ':53'; then
+    # Check if piholedoh service is up using docker-compose
+    if docker-compose -f docker-compose.piholedoh.yml ps | grep -q Up; then
+        echo "Error: pihole is already running. Exiting..."
+        exit 1
+    # Otherwise another process claimed it
+    else
+        echo "Error: Port 53 is already claimed by another process. Please check the documentation. Exiting..."
+        exit 1
+    fi
+fi
+
+# Create traefik_proxy external network if it doesn't exist yet
+docker network ls | grep traefik_proxy || docker network create traefik_proxy
+
+# Check and restart for docker-compose.piholedoh.yml
+if ! docker-compose -f docker-compose.piholedoh.yml ps | grep -q Up; then
+    echo "Restarting services defined in docker-compose.piholedoh.yml..."
+    docker-compose -f docker-compose.piholedoh.yml up -d
+fi
+
+# Check and restart for docker-compose.traefik.yml
+if ! docker-compose -f docker-compose.traefik.yml ps | grep -q Up; then
+    echo "Restarting services defined in docker-compose.traefik.yml..."
+    docker-compose -f docker-compose.traefik.yml up -d
+fi
